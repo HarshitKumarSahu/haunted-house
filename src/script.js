2ee6c8d83128fabb1fe16dcc9467df4b0c584449
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-09-25 19:14:22 +0530

    added graves

--- src/script.js
+++ src/script.js
@@ -100,6 +100,33 @@ bush4.position.set(- 1, 0.05, 2.6)
 
 house.add(bush1, bush2, bush3, bush4)
 
+// Graves
+const graves = new THREE.Group()
+scene.add(graves)
+
+const gravesGeo = new THREE.BoxGeometry(0.6, 0.8, 0.2)
+const gravesMat = new THREE.MeshStandardMaterial({color: "brown"})
+
+for(let i=0; i<30; i++) {
+
+    const angle = Math.random() * Math.PI * 2
+    const radius = 3.125 + Math.random() * 4
+    const x = Math.sin(angle) * radius
+    const z = Math.cos(angle) * radius
+    
+    const grave = new THREE.Mesh(gravesGeo, gravesMat);
+    grave.position.x = x
+    grave.position.y = Math.random() * 0.4
+    grave.position.z = z
+
+    grave.rotation.x =  (Math.random() - 0.5) * 0.4
+    grave.rotation.y =  (Math.random() - 0.5) * 0.4
+    grave.rotation.z =  (Math.random() - 0.5) * 0.4
+
+    graves.add(grave)
+}
+
+
 
 /**
  * Lights
